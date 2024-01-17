54715a78fb3082f0cf45eedafa5b5e81b12f80e9
Pablo Zamora <pablo.zamora@icloud.com>
2024-01-17 00:26:17 -0300

    correccion de errores

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import express, { urlencoded, json } from "express";
-import productsRouter from "./src/routes/products.router.js"
-import cartRouter from "./src/routes/cart.router.js"
+import productsRouter from "./src/routes/products.router.js";
+import cartRouter from "./src/routes/cart.router.js";
 
 const app = express();
 const PUERTO = 8080;
-
-
 
 //Middleware
 app.use(urlencoded({ extended: true }));
